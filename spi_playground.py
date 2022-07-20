0ec2a9287824298adfb1c0db85104fd504195bd4
bscobe4 <78442276+bscobe4@users.noreply.github.com>
2022-07-19 17:11:50 -0700

    Add files via upload

---
+++ spi_playground.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import spidev
+
+ESC = False
+numbytes = 1
+data = []
+mode = 1
+lsbfirst = False
+while(not ESC):
+
+    inp = input('\nHit any key and "enter" to input bytes you want to write to the SPI bus or type "op" for more options\n')
+    #print(hex(inp))
+    
+    if inp == 'op':
+        print("\n 'bytes' --> number of bytes to write.\n 'spimode' --> spi mode (0,1,2 or 3)\n 'lsb' --> set to LSB\n 'msb' --> set to MSB\n 'back' --> return to last page\n")
+        op = input("\nEnter command:\n")
+        if op == 'bytes':
+            numbytes = int(input("\nEnter the bumber of bytes you want to send:\n"))
+        if op == 'spimode':
+            mode = int(input("\nEnter mode(0,1,2 or 3):\n"))
+        if op == 'lsb':
+            lsbfirst = True
+        if op == 'msb':
+            lsbfirst = False
+        if op == 'back':
+            print("\nBack\n")
+    else:
+        
+        bytecount = 0
+        data = []
+        while(bytecount < numbytes):
+            data.append(int(input("\nEnter byte" + str(bytecount) + ":\n"),16))
+            bytecount = bytecount + 1
+            
+        spi.open(0,1)
+        spi.mode = mode
+        spi.max_speed_hz = 100000
+        spi.lsbfirst = lsbfirst
+        regs4 =spi.xfer2(data) #Read ll 16 registers starting with "ID"
+        print("\nData Out:\n")
+        print(data)
+        print("\nData In:\n")
+        print(regs4)
+        spi.close()
+
